b0169cf06d0bb4a5299b7cee9bbce907a29fee84
Jenny Lou Atico <jenny.atico@ph.ibm.com>
2021-05-10 11:45:27 +0800

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// create express app
+const app = express();
+
+// route handlers
+app.get('/', (req, res) => {
+    res.send({ hi: 'there '});
+});
+
+// listener
+const PORT = process.env.PORT || 5000; // dev default 5000
+app.listen(PORT);
